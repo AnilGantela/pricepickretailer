d0de3e7d030edbadfb082a0dc5a079b9e9338064
unknown <anilkumar.gantela77@gmail.com>
2025-02-24 22:01:08 +0530

    hello

--- src/assets/Components/About/index.jsx
+++ src/assets/Components/About/index.jsx
@@ -103,7 +103,7 @@ const RetailerDetails = () => {
           <img
             src={details.photo}
             alt="Retailer Photo"
-            className="w-full h-full object-cover rounded-lg shadow-md"
+            className="w-[90%] h-full object-cover rounded-lg shadow-md"
           />
         </div>
 
